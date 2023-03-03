e3a36baac1918f50c9c580d42d18af06f5db66c7
dever-cube <developed.by.fox@gmail.com>
2023-03-03 20:24:43 +0100

    init

---
+++ src/main/java/net/devcube/writery/Writery.java
@@ -0,0 +1,10 @@
+package net.devcube.writery;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Writery implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
